3f4de3c2978c267bf5b96783ad3b37c9bcba5e32
Thai Radin <thairadin@outlook.com>
2023-12-21 12:32:15 -0500

    hero, cta, and buttons

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,7 +5,7 @@
 <header class="flex flex-col relative z-20">
     <div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
         <a href="/">
-            <h1>
+            <h1 class="font-semibold">
                 Swoley
                 <span class="text-indigo-400">Moley</span>
             </h1>
